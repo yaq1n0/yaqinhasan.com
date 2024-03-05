af0c0f5d1dacf988709a0cfb25e80f5a3e265b66
yaq1n0 <62781980+yaq1n0@users.noreply.github.com>
2024-03-05 05:25:44 +0000

    upgraded projects navigation

---
+++ js/projects.js
@@ -0,0 +1,24 @@
+// Javascript file for projects.html
+
+var project_ids = ['project0', 'project1', 'project2', 'project3', 'project4', 'project5'];
+
+function openAll() {
+    let elements = document.getElementsByClassName('collapsible-projects');
+    for (var i=0; i<elements.length; i++) {
+        elements[i].open = true;
+    }
+}
+
+function closeAll() {
+    let elements = document.getElementsByClassName('collapsible-projects');
+    for (var i=0; i<elements.length; i++) {
+        elements[i].open = false;
+    }
+}
+
+function gotoProject(project_id) {
+    document.getElementById(project_id).open = true;
+    document.getElementById(project_id).scrollIntoView();
+}
+
+closeAll()
