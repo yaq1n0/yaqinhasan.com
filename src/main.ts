9afc0d8990f932ba8956c1df528a655d9e2ae451
yaq1n0 <62781980+yaq1n0@users.noreply.github.com>
2025-06-07 01:12:46 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-/* Import Font Awesome */
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 
-/* Import Swiper CSS */
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 
-/* Add entire icon libraries to the library */
 library.add(fas, fab);
 
 const app = createApp(App);
@@ -23,12 +20,13 @@ app.component("FontAwesomeIcon", FontAwesomeIcon);
 app.use(router);
 
 // Handle initial route for SSG pages
-if (window.__INITIAL_ROUTE__ && window.__INITIAL_ROUTE__ !== "/") {
-  // Wait for router to be ready before setting the route
-  router.isReady().then(() => {
-    router.push(window.__INITIAL_ROUTE__);
-    app.mount("#app");
-  });
-} else {
+const initialRoute = window.__INITIAL_ROUTE__;
+// async function to handle initial route, is blocking though!
+(async () => {
+  if (initialRoute && initialRoute !== "/") {
+    // Wait for router to be ready before setting the route
+    await router.isReady();
+    router.push(initialRoute);
+  }
   app.mount("#app");
-}
+})();
