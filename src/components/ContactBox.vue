1425a819c8493be7c4119bc16945f407f5fdfa72
yaq1n0 <62781980+yaq1n0@users.noreply.github.com>
2025-11-29 00:45:35 +0000

    refactor ContactBox to use ContactItem props

--- src/components/ContactBox.vue
+++ src/components/ContactBox.vue
@@ -1,63 +1,16 @@
 <template>
   <div class="contact-box">
-    <g-button
-      v-if="showItem('cv')"
-      label="CV"
-      to="/contact"
-      icon="file-alt"
-      icon-size="lg"
-      label-pos="below"
-      shape="squircle"
-      border="none"
-      background="transparent"
-    />
-    <g-button
-      v-if="showItem('linkedin')"
-      label="LinkedIn"
-      href="https://www.linkedin.com/in/yaqinhasan"
-      icon="linkedin"
-      icon-prefix="fab"
-      icon-size="lg"
-      label-pos="below"
-      shape="squircle"
-      border="none"
-      background="transparent"
-    />
-    <g-button
-      v-if="showItem('email')"
-      label="Email"
-      href="mailto:yaqin.k.hasan@gmail.com"
-      icon="envelope"
-      icon-size="lg"
-      label-pos="below"
-      shape="squircle"
-      border="none"
-      background="transparent"
-    />
-    <g-button
-      v-if="showItem('github')"
-      label="GitHub"
-      href="https://github.com/yaq1n0"
-      icon="github"
-      icon-prefix="fab"
-      icon-size="lg"
-      label-pos="below"
-      shape="squircle"
-      border="none"
-      background="transparent"
-    />
+    <g-button v-for="(item, index) in items" :key="index" v-bind="item" label-pos="below" shape="squircle" border="none" background="transparent" />
   </div>
 </template>
 
 <script setup lang="ts">
 import GButton from "@/components/GButton.vue";
+import type { ContactItem } from "../data/models/Button";
 
-type ContactItem = "cv" | "linkedin" | "email" | "github";
-const props = defineProps<{
-  items?: ContactItem[];
+defineProps<{
+  items: ContactItem[];
 }>();
-
-const showItem = (item: ContactItem) => (props.items ? props.items.includes(item) : true);
 </script>
 
 <style lang="scss" scoped>
